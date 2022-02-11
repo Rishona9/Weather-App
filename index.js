f886c78abb35f0cc3864fadfa91423bda350d67b
Rishona9 <96752903+Rishona9@users.noreply.github.com>
2022-02-11 05:39:29 -0500

    Fixed JS line 99

--- index.js
+++ index.js
@@ -95,12 +95,11 @@ function displayWeatherConditions(response) {
   document.querySelector(".dayTime").innerHTML = formatDate(
     response.data.dt * 1000
   );
-  let iconElement = document
-    .querySelector("#main-icon")
-    .setAttribute(
-      "src",
-      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-    );
+  let iconElement = document.querySelector("#main-icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 
   iconElement.setAttribute("alt", response.data.weather[0].description);
   getForecast(response.data.coord);
